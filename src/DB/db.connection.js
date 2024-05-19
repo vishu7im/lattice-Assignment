190744f6ddd6bbe9998effbc086899b54f2bba2f
vishu7im <vishu.im.0007@gmail.com>
2024-05-19 11:36:54 +0530

    readme.md

--- src/DB/db.connection.js
+++ src/DB/db.connection.js
@@ -3,12 +3,13 @@ const mysql = require("mysql2/promise");
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
+const database = process.env.DB_Database;
 
 const dbConfig = {
   host,
   user,
   password,
-  database: "lattice",
+  database,
 };
 
 const DB = mysql.createPool(dbConfig);
